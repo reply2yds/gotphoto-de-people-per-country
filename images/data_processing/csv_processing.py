146e0d227b68df68c98c672105a4a5b7bac831c6
Yagya <reply2yds@gmail.com>
2022-12-30 12:21:18 +0100

    Handle exception

--- images/data_processing/csv_processing.py
+++ images/data_processing/csv_processing.py
@@ -2,75 +2,128 @@
 
 import csv
 import json
-import sqlalchemy
+import time
+from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy import create_engine, schema
 
 
-def csv_load():
+def db_connect():
     # connect to the database
-    engine = sqlalchemy.create_engine("mysql+mysqldb://codetest:swordfish@localhost:3306/codetest", pool_recycle=3600, encoding='utf-8', echo=False)
-    connection = engine.connect()
-    metadata = sqlalchemy.schema.MetaData(engine)
-
-    # make an ORM object to refer to the table
-    get_examples = sqlalchemy.schema.Table(
-        "examples", metadata, autoload=True, autoload_with=engine
-    )
-    get_places = sqlalchemy.schema.Table(
-        "places", metadata, autoload=True, autoload_with=engine
-    )
-    get_peoples = sqlalchemy.schema.Table(
-        "peoples", metadata, autoload=True, autoload_with=engine
+    engine = create_engine(
+        "mysql://codetest:swordfish@database/codetest",
+        pool_recycle=3600,
+        encoding="utf-8",
+        echo=False,
+        pool_pre_ping=True,
     )
 
-    with open("/data/input/example.csv") as csv_file:
-        reader = csv.reader(csv_file)
-        next(reader)
-        for row in reader:
-            connection.execute(
-                get_examples.insert().values(name=row[0])
-            )
+    try:
+        print("MySQL Engine:", engine)
+        time.sleep(10)
+        engine.connect()
+        print("Successfully connected to MySQL database!!!")
+        return engine
+    except SQLAlchemyError as err:
+        error = str(err.__dict__["orig"])
+        print(error)
 
-    with open("/data/input/places.csv") as csv_file:
-        reader = csv.reader(csv_file)
-        next(reader)
-        for row in reader:
-            connection.execute(
-                get_places.insert().values(city=row[0], county=row[1], country=row[2])
-            )
 
-    with open("/data/input/people.csv") as csv_file:
-        reader = csv.reader(csv_file)
-        next(reader)
-        for row in reader:
-            connection.execute(
-                get_peoples.insert().values(
-                    given_name=row[0],
-                    family_name=row[1],
-                    date_of_birth=row[2],
-                    place_of_birth=row[3],
-                )
-            )
+def load_places(engine):
+    metadata = schema.MetaData(engine)
+    get_places = schema.Table("places", metadata, autoload=True, autoload_with=engine)
+    try:
+        with engine.connect() as connection:
+            with open("/data/input/places.csv") as csv_file:
+                reader = csv.reader(csv_file)
+                next(reader)
+                for row in reader:
+                    connection.execute(
+                        get_places.insert().values(
+                            city=row[0], county=row[1], country=row[2]
+                        )
+                    )
+    except:
+        raise
 
-def people_per_country_json():
-    """output SQL-result to a JSON file"""
 
-    engine = sqlalchemy.create_engine("mysql+mysqldb://codetest:swordfish@localhost:3306/codetest", pool_recycle=3600, encoding='utf-8', echo=True)
-    connection = engine.connect()
-    metadata = sqlalchemy.schema.MetaData(engine)
+def load_peoples(engine):
+    metadata = schema.MetaData(engine)
+    get_peoples = schema.Table("peoples", metadata, autoload=True, autoload_with=engine)
+    try:
+        with engine.connect() as connection:
+            with open("/data/input/people.csv") as csv_file:
+                reader = csv.reader(csv_file)
+                next(reader)
+                for row in reader:
+                    connection.execute(
+                        get_peoples.insert().values(
+                            given_name=row[0],
+                            family_name=row[1],
+                            date_of_birth=row[2],
+                            place_of_birth=row[3],
+                        )
+                    )
+    except:
+        raise
 
-    #consider using ORM to join tables
-    sql_statement = 'select country, count(1) num_of_people from places left join peoples on lower(place_of_birth) = lower(city) group by country'
 
-    with open('/data/sample_output.json', 'w') as json_file:
-        rows = connection.execute(sql_statement).fetchall()
-        data_dict = {}
-        for row in rows:
-            data_dict[row[0]] = row[1]
-        json.dump(data_dict, json_file, separators=(',', ':'))
+def load_places_info(engine):
+    sql = """
+    insert into places_info (city, county, country)
+    select city, county, country from places
+    """
+    try:
+        with engine.connect() as connection:
+            connection.execute(sql)
+    except:
+        raise
+
+
+def load_peoples_info(engine):
+    sql = """
+    insert into peoples_info (given_name, family_name, date_of_birth, birth_place_id)
+    select given_name, family_name, date_of_birth, pi.id
+    from peoples p 
+    left join places_info pi
+    on lower(city) = lower(place_of_birth)
+    """
+    try:
+        with engine.connect() as connection:
+            connection.execute(sql)
+    except:
+        raise
+
+
+def people_per_country_json(engine):
+    try:
+        with engine.connect() as connection:
+            # consider using ORM to join tables
+            sql_statement = """
+            select country, count(1) num_of_people 
+            from places_info pi1
+            left join peoples_info pi2
+            on birth_place_id = pi1.id 
+            group by country
+            order by num_of_people desc
+            """
+
+            with open("/data/sample_output.json", "w") as json_file:
+                rows = connection.execute(sql_statement).fetchall()
+                data_dict = {}
+                for row in rows:
+                    data_dict[row[0]] = row[1]
+                json.dump(data_dict, json_file, separators=(",", ":"))
+    except:
+        raise
+
 
 def main():
-    csv_load()
-    people_per_country_json()
+    engine = db_connect()
+    load_places(engine)
+    load_peoples(engine)
+    load_places_info(engine)
+    load_peoples_info(engine)
+    people_per_country_json(engine)
 
 
 if __name__ == "__main__":
--- sql_scripts/init.sql
+++ sql_scripts/init.sql
@@ -1,14 +1,14 @@
-drop table if exists examples;
-
-create table `examples` (
-  `id` int not null auto_increment,
-  `name` varchar(80) default null,
-  primary key (`id`)
-);
-
 drop table if exists places;
 
 create table `places` (
+  `city` varchar(80) default null,
+  `county` varchar(80) default null,
+  `country` varchar(80) default null
+);
+
+drop table if exists places_info;
+
+create table `places_info` (
   `id` int not null auto_increment,
   `city` varchar(80) default null,
   `county` varchar(80) default null,
@@ -20,10 +20,20 @@ create table `places` (
 drop table if exists peoples;
 
 create table `peoples` (
+  `given_name` varchar(80) default null,
+  `family_name` varchar(80) default null,
+  `date_of_birth` date default null,
+  `place_of_birth` varchar(80) default null
+);
+
+drop table if exists peoples_info;
+
+create table `peoples_info` (
   `id` int not null auto_increment,
   `given_name` varchar(80) default null,
   `family_name` varchar(80) default null,
   `date_of_birth` date default null,
-  `place_of_birth` varchar(80) default null,
+  `birth_place_id` int default null,
+  FOREIGN KEY (birth_place_id) REFERENCES places_info(id),
   primary key (`id`)
 );
